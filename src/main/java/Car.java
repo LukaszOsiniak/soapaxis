db424a32ffda2dd3a236e87d2c4a6cb168e09ea3
LukaszOsiniak <lukasz.osiniak@gmail.com>
2021-07-01 12:40:18 +0200

    Initial commit

---
+++ src/main/java/Car.java
@@ -0,0 +1,8 @@
+public class Car {
+
+    protected String name;
+    protected String owner;
+    protected String licensePlateNumber;
+    protected String yearOfProduction;
+    protected boolean isInsured;
+}
